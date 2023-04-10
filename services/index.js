bf14fc7ce66d494abdd2ff7424c20269804ed93a
jareddwilson32 <jareddwilson32@gmail.com>
2023-04-10 15:17:47 -0600

    reorganization to include services

---
+++ services/index.js
@@ -0,0 +1,96 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const DB = require('./database');
+const { PeerProxy } = require('./peerProxy.js');
+
+const authCookieName = 'token';
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+// createAuthtoken for a new user
+apiRouter.post('/auth/register', async (req, res) => {
+    if (await DB.getUser(req.body.username)) {
+        res.status(409).send({ msg: 'Existing User' });
+    }
+    else {
+        const user = await DB.createUser(req.body.username, req.body.password);
+
+        // Set the cookie
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id: user._id,
+        });
+    }
+});
+
+// getAuthtoken for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.username);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ id: user._id });
+            return;
+        }
+    }
+    else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+});
+
+// GetUser
+apiRouter.get('/user/:username', async (req, res) => {
+    const user = await DB.getUser(req.params.username);
+    if (user) {
+        const token = req?.cookies.token;
+        res.send({ username: user.username, authenticated: token === user.token });
+        return
+    }
+    else {
+        res.status(404).send({ msg: 'Unknown User'});
+    }
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+const httpService = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+new PeerProxy(httpService);
+
+
